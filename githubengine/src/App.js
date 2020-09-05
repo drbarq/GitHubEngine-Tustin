950996c30f348c27703383a7074a0a648cd9d628
Joe Tustin <J.Tustin@gmail.com>
2020-09-05 13:13:57 -0600

    adding in server and inital render

--- githubengine/src/App.js
+++ githubengine/src/App.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [state, setState] = useState({ data: null });
+
+  useEffect(() => {
+    callBackEnd()
+      .then((res) => setState({ data: res.express }))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const callBackEnd = async () => {
+    const response = await fetch("/express_backend");
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message);
+    } else {
+      return body;
+    }
+  };
+
+  return (
+    <div className="App">
+      <h4>State.data: {state.data}</h4>
+    </div>
+  );
 }
 
 export default App;
