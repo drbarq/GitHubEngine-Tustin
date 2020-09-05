83d30bff9244045879b3dc653e8564c201d223d1
Joe Tustin <J.Tustin@gmail.com>
2020-09-05 13:29:07 -0600

    testing github endpoint

--- server/server.js
+++ server/server.js
@@ -4,6 +4,20 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const gitHubRepoSearchURL = "https://api.github.com/search/repositories";
+// header: { Accept: application/vnd.github.mercy-preview+json}
+
+/**
+Limitations on query length
+
+The Search API does not support queries that:
+
+    are longer than 256 characters (not including operators or qualifiers).
+    have more than five AND, OR, or NOT operators.
+
+These search queries will return a "Validation failed" error message.
+ */
+
 // console log to check if server running
 
 app.listen(port, () => console.log(`Listening on Port ${port}`));
