7cba6be9de36b8c9d2bbf680045f045015398d5b
Joe Tustin <J.Tustin@gmail.com>
2020-09-05 22:07:49 -0600

    adding items

--- githubengine/src/components/Search/components/searchBar/components/searchResultRenders/index.js
+++ githubengine/src/components/Search/components/searchBar/components/searchResultRenders/index.js
@@ -7,14 +7,22 @@ const SearchResultRenders = ({ searchResults, setSearchResults }) => {
     data: { total_count, incomplete_results, items },
   } = searchResults;
 
+  // console.log(items);
+
   const [sort, setSort] = useState({
     name: "",
     accend: null,
+    filterLanguage: "all",
+    items: [],
   });
 
   useEffect(() => {
     updateCurrentSort();
   }, [sort.name]);
+
+  useEffect(() => {
+    filterRepoByLanguage();
+  }, [sort.filterLanguage]);
 
   const sortData = () => {
     return items.sort((a, b) => {
@@ -22,17 +30,9 @@ const SearchResultRenders = ({ searchResults, setSearchResults }) => {
     });
   };
 
-  // const sortItemsDirection = () => {
-  //   if (sort.accend) {
-  //     sortData();
-  //   } else {
-  //     sortData().reverse();
-  //   }
-  // };
-
   const updateCurrentSort = (atttributeName = sort.name) => {
     sort.accend ? sortData() : sortData().reverse();
-    setSort({ name: atttributeName, accend: !sort.accend });
+    setSort({ ...sort, name: atttributeName, accend: !sort.accend });
   };
 
   const TableHeaderLabel = () => {
@@ -74,9 +74,52 @@ const SearchResultRenders = ({ searchResults, setSearchResults }) => {
     });
   };
 
+  const itemLanguages = () => {
+    let languages = [];
+    items.forEach((repo) => {
+      return languages.includes(repo.language)
+        ? null
+        : languages.push(repo.language);
+    });
+    return languages;
+  };
+
+  const filterRepoByLanguage = () => {
+    let filtered = items.filter((repo) => {
+      return repo.language === sort.filterLanguage;
+    });
+
+    setSort({ ...sort, items: filtered });
+  };
+
+  const generateLanguageSelections = () => {
+    const handleSelection = (e) => {
+      setSort({ ...sort, filterLanguage: e.target.value });
+      // filterRepoByLanguage();
+    };
+
+    let languageSelections = [<option value="all">--</option>];
+
+    itemLanguages().forEach((language, index) => {
+      languageSelections.push(
+        <option key={index} value={language}>
+          {language}
+        </option>
+      );
+    });
+
+    return (
+      <select onChange={handleSelection} value={sort.filterLanguage}>
+        {languageSelections}
+      </select>
+    );
+  };
+
   return (
     <div className="SearchResultsRender-container">
       <h1>Searched Term: {searchedTerm}</h1>
+      <h4>Filter By:</h4>
+      {generateLanguageSelections()}
       <table className="results-table">
         <thead>
           <tr>{TableHeaderLabel()}</tr>
