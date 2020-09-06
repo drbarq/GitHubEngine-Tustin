54e111351262105f7a645c33de65bd256fea0f92
Joe Tustin <J.Tustin@gmail.com>
2020-09-06 17:03:59 -0600

    adding in navigation

--- githubengine/src/components/Details/index.js
+++ githubengine/src/components/Details/index.js
@@ -37,32 +37,36 @@ const Details = ({ searchedRepos }) => {
 
   return (
     <div className="Details-container">
-      <Link to="/">Home</Link>
-      {/* <h1>Repo Details</h1> */}
       <div className="headerLink-container">
         <img src={avatar_url} />
-        <div className="owner">
-          <a href={html_url} target="_blank">
-            {login}/
-          </a>
+        <div className="gihubLink">
+          <div className="owner">
+            <a href={owner_html_url} target="_blank">
+              {login}/
+            </a>
+          </div>
+          <div className="repoName">
+            <a href={html_url} target="_blank">
+              {name}
+            </a>
+          </div>
         </div>
-        <div className="repoName">
-          <a href={html_url} target="_blank">
-            /{name}
-          </a>
-        </div>
-        {/* <h3>
-          <a href={owner_html_url} target="_blank">
-            {login}
-          </a>{" "}
-          /
-        </h3>
-        <h3>
-          /<a href={html_url}> {name}</a>
-        </h3> */}
+        <Link to="/">
+          <div className="navContainer">
+            <i class="fas fa-home"></i>
+            <div className="title">Home</div>
+          </div>
+        </Link>
       </div>
       <div className="about">
-        <p>About: {description}</p>
+        <p>
+          About: <strong>{description}</strong>{" "}
+        </p>
+      </div>
+      <div className="language">
+        <p>
+          Language: <strong>{language}</strong>
+        </p>
       </div>
       <div className="repoStats-container">
         <div className="stat">
@@ -73,6 +77,14 @@ const Details = ({ searchedRepos }) => {
         </div>
         <div className="stat">
           <i class="far fa-eye"> {watchers}</i>
+        </div>
+      </div>
+      <div className="createdStats-container">
+        <div className="created">
+          Created at: <strong>{created_at}</strong>{" "}
+        </div>
+        <div className="created">
+          Updated at: <strong>{updated_at}</strong>{" "}
         </div>
       </div>
     </div>
