fe04c682066a4776b63508c44028d4b9d97f91f5
Joe Tustin <J.Tustin@gmail.com>
2020-09-06 18:17:29 -0600

    Removing unused files

--- githubengine/src/components/Search/components/searchResultRenders/components/TableHeaderLabel/index.js
+++ githubengine/src/components/Search/components/searchResultRenders/components/TableHeaderLabel/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./styles.scss";
 
 const TableHeaderLabel = ({ sort, setSort }) => {
   const repoAttributes = [
--- githubengine/src/components/Search/components/searchResultRenders/index.js
+++ githubengine/src/components/Search/components/searchResultRenders/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import "./styles.scss";
-import { Link } from "react-router-dom";
 
 import TableHeaderLabel from "./components/TableHeaderLabel";
 import TableDataRow from "./components/TableDataRow";
